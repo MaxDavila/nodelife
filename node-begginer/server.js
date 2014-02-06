9ddd534209e5dc171baea0112e61de58f961422b
maxdavila <adavila0711@gmail.com>
2014-02-05 23:05:37 -0800

    add export fn

--- node-begginer/server.js
+++ node-begginer/server.js
@@ -1,7 +1,14 @@
 var http = require('http');
 
-http.createServer(function(req, res){
-	res.writeHead(200, { 'Content-Type' : 'text/plain'});
-	res.write('wudap');
-	res.end();
-}).listen(8888);
+function start(){
+	http.createServer(function(req, res){
+		console.log('request received')
+		res.writeHead(200, { 'Content-Type' : 'text/plain'});
+		res.write('wudap');
+		res.end();
+	}).listen(8888);
+
+	console.log('server has started yo!');
+}
+
+exports.start = start;
