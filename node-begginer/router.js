c7f6504c39640e05d4784ea93377366617564d72
maxdavila <adavila0711@gmail.com>
2014-02-07 00:25:16 -0800

    checks if request handler fn exists

--- node-begginer/router.js
+++ node-begginer/router.js
@@ -1,5 +1,10 @@
-function route(pathname){
+function route(handle, pathname){
 	console.log('bout to route to' + pathname);
+	if (typeof handle[pathname] === 'function')
+		handle[pathname]();
+	else
+		console.log('no request handler found for' + pathname);
+
 }
 
 exports.route = route;
