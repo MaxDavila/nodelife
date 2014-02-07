619567d637a7998fa5b73398251c7da60ef46d44
maxdavila <adavila0711@gmail.com>
2014-02-06 23:45:10 -0800

    get pathname from request

--- node-begginer/server.js
+++ node-begginer/server.js
@@ -1,8 +1,10 @@
 var http = require('http');
+var url = require('url');
 
 function start(){
 	http.createServer(function(req, res){
-		console.log('request received')
+		var pathname = url.parse(req.url).pathname;
+		console.log('Request for' + pathname + 'received')
 		res.writeHead(200, { 'Content-Type' : 'text/plain'});
 		res.write('wudap');
 		res.end();
