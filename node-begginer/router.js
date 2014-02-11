1be8c6fc017841a7feff89c105e0e893e0f8ef74
maxdavila <adavila0711@gmail.com>
2014-02-11 00:20:56 -0800

    pass response object to req handler or handle 404

--- node-begginer/router.js
+++ node-begginer/router.js
@@ -1,10 +1,15 @@
-function route(handle, pathname){
+function route(handle, pathname, response){
 	console.log('bout to route to' + pathname);
-	if (typeof handle[pathname] === 'function')
-		return handle[pathname]();
-	else
+	
+	if (typeof handle[pathname] === 'function') {
+		return handle[pathname](response);
+	} else {
 		console.log('no request handler found for' + pathname);
-		return "404 not found"
+		response.writeHead(404, {'Content-Type' : 'text/plain'});
+		response.write("Not found");
+		response.end();
+	}
+
 }
 
 exports.route = route;
