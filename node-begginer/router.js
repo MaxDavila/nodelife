b8ba6829ee6e6b7d94cd6ab0be9fb06b1aebac0c
maxdavila <adavila0711@gmail.com>
2014-02-11 01:15:46 -0800

    pass postdata to handler

--- node-begginer/router.js
+++ node-begginer/router.js
@@ -1,8 +1,8 @@
-function route(handle, pathname, response){
+function route(handle, pathname, response, postData){
 	console.log('bout to route to' + pathname);
 	
 	if (typeof handle[pathname] === 'function') {
-		return handle[pathname](response);
+		return handle[pathname](response, postData);
 	} else {
 		console.log('no request handler found for' + pathname);
 		response.writeHead(404, {'Content-Type' : 'text/plain'});
